0789bcc4a710b7cc4cb42b6380a385bed039c9e8
TheRaiymbekov <irajymbekov8@gmail.com>
2024-09-12 14:00:51 +0600

    Add files via upload

---
+++ DZ3/main.js
@@ -0,0 +1,64 @@
+// ЗАДАНИЕ 1
+// var num = (a,b) => {
+//     if(a>b) {
+//         console.log(b);
+//     }else if(a<b){
+//         console.log(a);
+//     }
+// }
+
+// num(45,56)
+
+// ЗАДАНИЕ 2
+// console.log((prompt().length));
+
+
+// str = prompt()
+//   function name(str) {
+//     console.log(str.length)
+//   }
+//   name(str)
+
+// ЗАДАНИЕ 3 
+// function calc(str) {
+
+//     const [num1, operator, num2] = str.split(' ');
+  
+//     const a = Number(num1); 
+  
+//     const b = Number(num2);
+  
+  
+//     let alert;
+  
+//     switch (operator) {
+  
+//       case '+':
+//         alert = a + b;
+//         break;
+  
+//       case '-':
+//         alert = a - b;
+//         break;
+  
+//       case '*':
+//         alert = a * b;
+//         break;
+  
+//       case '/':
+//         if (b === 0) {
+//           console.log("На ноль делить нельзя");
+//         }
+  
+//         alert = a / b;
+//         break;
+  
+//       default:
+//         console.log('Неправильный оператор');
+    
+//     }
+  
+//     return str(result);
+  
+//   }
+
